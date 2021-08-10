e4f18dc828165379eff87743f3f3d4617aaea144
abdullahhaidar <abudehhaidar8@gmail.com>
2021-08-10 14:44:16 +0200

    fix typo

--- hello-mars/src/components/todayWeather.js
+++ hello-mars/src/components/todayWeather.js
@@ -6,7 +6,7 @@ const TodayWeather = (props) => {
             <h3>this is todays weather</h3>
             <ul>
                 <li>Date: {props.todayWeather.date}</li>
-                <li>SOl: {props.todayWeather.sol}</li>
+                <li>Sol: {props.todayWeather.sol}</li>
                 <li>Season: {props.todayWeather.season}</li>
                 <li>Min Temp: {props.todayWeather.minTemp} °C</li>
                 <li>Max Temp: {props.todayWeather.maxTemp} °C</li>
