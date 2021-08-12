ccbb1e8739fef8ada5cbc30293f0ef9c436dad60
abdullahhaidar <abudehhaidar8@gmail.com>
2021-08-12 10:26:05 +0200

    setting up css classes and postions

--- hello-mars/src/components/fiveDaysWeather.js
+++ hello-mars/src/components/fiveDaysWeather.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import './fiveDaysWeather.css'
+
 
 
 const FiveDaysWeather = (props) => {
@@ -6,21 +8,21 @@ const FiveDaysWeather = (props) => {
     console.log(props)
     return (
         <div>
-            <h3>this is five day weather</h3>
-
-            {props.fiveDaysWeather ? props.fiveDaysWeather.map((element, index) =>
-                <ul>
-                    {/* <li key={index}></li> */}
-                    <li>Date: {element.date}</li>
-                    <li>Sol: {element.sol}</li>
-                    <li>Season: {element.season}</li>
-                    <li>Min Temp: {element.minTemp}</li>
-                    <li>Max Temp: {element.maxTemp}</li>
-                    <li>Opacity: {element.opacity}</li>
-                    <li>Air Pressure: {element.airPressure}</li>
-                </ul>
-            ) : <p>there is no data</p>
-            }
+            <h3 className="fiveDaysHeadline">The past five Days</h3>
+            <div className="fiveDaysContainer">
+                {props.fiveDaysWeather ? props.fiveDaysWeather.map((element, index) =>
+                    <ul className="listContainer">
+                        {/* <li key={index}></li> */}
+                        <li>Date: {element.date}</li>
+                        <li>Sol: {element.sol}</li>
+                        <hr />
+                        <img src="" alt="graphic representation of the weather" />
+                        <li>Min Temp: {element.minTemp}</li>
+                        <li>Max Temp: {element.maxTemp}</li>
+                    </ul>
+                ) : <p>there is no data</p>
+                }
+            </div>
         </div>
     )
 }
--- hello-mars/src/components/todayWeather.js
+++ hello-mars/src/components/todayWeather.js
@@ -1,18 +1,29 @@
 import React from 'react'
+import './todayWeather.css'
+
 
 const TodayWeather = (props) => {
+
     return (
-        <div>
-            <h3>this is todays weather</h3>
-            <ul>
-                <li>Date: {props.todayWeather.date}</li>
-                <li>Sol: {props.todayWeather.sol}</li>
-                <li>Season: {props.todayWeather.season}</li>
-                <li>Min Temp: {props.todayWeather.minTemp} °C</li>
-                <li>Max Temp: {props.todayWeather.maxTemp} °C</li>
-                <li>Opacity: {props.todayWeather.opacity}</li>
-                <li>Air Pressure: {props.todayWeather.airPressure}</li>
-            </ul>
+        <div className="containsTodayWeatherAndLog">
+            <div className='todayWeatherContainer'>
+                <h3>Gale Crater, Mars</h3>
+                <ul className='todayWeatherListItem'>
+                    <li>Season: {props.todayWeather.season}</li>
+                    <li>Sol: {props.todayWeather.sol}</li>
+                    <li>Date: {props.todayWeather.date}</li>
+                    <hr />
+                    <li>Min Temp: {props.todayWeather.minTemp} °C</li>
+                    <li>Max Temp: {props.todayWeather.maxTemp} °C</li>
+                    <li>Opacity: {props.todayWeather.opacity}</li>
+                    <img src="" alt="graphic representation of the weather" />
+                    <li>Air Pressure: {props.todayWeather.airPressure}</li>
+                </ul>
+            </div>
+            <div>
+                <h2 className="logoContainer">HELLO MARS</h2>
+                <img className="logImage" src="" alt="planet Mars" />
+            </div>
         </div>
     )
 }
